297be5341ba3a5cac0f41431d881658f86c5b041
Bela Bursan <burszan@gmail.com>
2025-04-03 11:52:39 +0200

    fix php log

--- server/lamp/data/www/html/src/utils/logger.php
+++ server/lamp/data/www/html/src/utils/logger.php
@@ -3,8 +3,6 @@
 define('I_ERROR', "ERR");
 define('I_INFO', "INF");
 $ONLY_ERRORS = false;
-$SPATH = "/var/log/php";
-$LOGFILE = "racesignal_php.log";
 
 /**
  * Writes a log message to the logfile defined in the config
@@ -12,16 +10,10 @@ $LOGFILE = "racesignal_php.log";
  * @param string type Type of the message, can be I_ERROR or I_INFO
  */
 function log_all($msg, $type=I_INFO) {
-    global $SPATH;
-    global $LOGFILE;
-
-    if (!file_exists("$SPATH/")) {
-        mkdir($SPATH, 0755, true);
-    }
-
     $now = date("Y-m-d H:i:s");
-    $logfile = "$SPATH/$LOGFILE";
-    error_log("$type($now) - $msg\n", 3, $logfile);
+    $out = fopen('php://stdout', 'w');
+    fwrite($out, "$type($now) - $msg\n");
+    fclose($out);
 }
 
 
