84ebb7f2e1794e0eef3ec031a038b79f6b6a7e4b
Bela Bursan <BUB@svep.se>
2025-04-04 12:38:06 +0200

    hello signal

---
+++ server/dispatcher/app/src/main/java/com/buri/App.java
@@ -0,0 +1,13 @@
+package com.buri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Signal!" );
+    }
+}
