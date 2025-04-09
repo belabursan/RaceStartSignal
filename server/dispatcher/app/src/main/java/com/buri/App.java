ad3fe45908254f72112846c4a433ccce3475f562
Bela Bursan <BUB@svep.se>
2025-04-09 13:22:07 +0200

    test in app

--- server/dispatcher/app/src/main/java/com/buri/App.java
+++ server/dispatcher/app/src/main/java/com/buri/App.java
@@ -4,7 +4,7 @@ import com.buri.config.Config;
 import com.buri.config.ConfigStatus;
 import com.buri.db.Db;
 import com.buri.db.DbFactory;
-import com.buri.signal.Signal;
+import com.buri.signal.SignalList;
 
 /**
  * Hello world!
@@ -22,11 +22,34 @@ public class App
 
             Config config = db.getConfig();
             System.out.println(config.toString());
-            
-            int x=0;
+            ConfigStatus status = db.getDbStatus();
+            SignalList list = null;
+
             while(true) {
-                System.out.println( "Hello Signal: " + x++ );
-                Thread.sleep(10000);
+                ConfigStatus statusTmp = db.getDbStatus();
+                if(list == null) {
+                    list = db.getSignalList();
+                    System.out.println("List size: " + list.size());
+                }
+
+                if(status.isDbChanged(statusTmp)) {
+                    System.out.println("Db status changed!!!");
+                    if(status.isListChanged(statusTmp)) {
+                        // handle list change
+                        //  1 abort signaling thread
+                        list = db.getSignalList();
+                        System.out.println("New list size: " + list.size());
+                        //start signaling with the new list
+                    } else {
+                        config = db.getConfig();
+                        //handle mute and paus
+                    }
+                    status = statusTmp;
+                    statusTmp = null;
+                } else {
+                    System.out.println("No change in db, sleeping...");
+                    Thread.sleep(5000);
+                }
             }
         } catch (InterruptedException e) {
             System.out.println("Thread interrupted: " + e.getMessage());
